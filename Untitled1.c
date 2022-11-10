a45b38cab696efa2e5b90bd9567b7201ea1bf86f
tamimmuheb <99010155+tamimmuheb@users.noreply.github.com>
2022-11-10 13:47:20 +0600

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+main()
+ {
+ float velocity,acceleration,deceleration,time;
+ printf("Enter velocity:\n");
+ scanf("%f",&velocity);
+ printf("Enter the time:\n");
+ scanf("%f",&time);
+ acceleration = velocity / time;
+ deceleration = -(velocity/time);
+ if (acceleration>0) {
+ printf("The acceleration is %f", acceleration);
+ }
+ else if (acceleration<0){
+ printf("The deceleration is %f",deceleration);
+ }
+ return 0;
+}
+
+
+
